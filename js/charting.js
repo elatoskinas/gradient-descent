840b73b008feb700a3ec21b8898dc5d1997ca30e
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-09-30 20:32:01 +0200

    Add code to create a Scatter Chart with random data

--- js/charting.js
+++ js/charting.js
@@ -2,12 +2,63 @@ var main = function ()
 {
     "use strict";
     
-    var chart = document.getElementById('chart');
-    
-    /*Plotly.plot( chart, [{
-        x: [1,2,3],
-        y: [1,2,4]
-    }]);*/
+    // Initialize a Chart with x and y axis
+    var chart = initializeChart('chart');
+
+    // Generate some random data
+    var data = getRandomData(20, {x: 0, y: 0}, {x: 10, y: 10});
+
+    addScatterDataToChart(chart, data);
+}
+
+function initializeChart(name) {
+    // Initialize margins
+    var margin = {top: 25, right: 25, bottom: 25, left: 25},
+        width = 400 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    // Create base Chart SVG
+    var svg = d3.select("#" + name).append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transorm", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Add X axis
+    var x = d3.scaleLinear().domain([0, 10]).range([0, width]);
+    svg.append("g").attr("transform", "translate(0," + width + ")")
+    .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear().domain([0, 10]).range([ height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    return {chart: svg, xAxis: x, yAxis: y};
+}
+
+function addScatterDataToChart(chart, data) {
+    chart.chart.append("g")
+        .selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) { return chart.xAxis(d.x); })
+        .attr("cy", function(d) { return chart.yAxis(d.y); })
+        .attr("r", 4)
+        .style("fill", "#0011bb");
+}
+
+function getRandomData(entries, min, max) {
+    var result = [];
+
+    for (var i = 0; i < entries; ++i) {
+        xVal = Math.random() * (max.x - min.x) + min.x;
+        yVal = Math.random() * (max.y - min.y) + min.y;
+        result.push({x : xVal, y: yVal});
+    }
+
+    return result;
 }
 
 // Define entry point for program (main function in this case)
