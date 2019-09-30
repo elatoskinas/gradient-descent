5e7a58ba8a098544c14a1a1255c7bb9d68ec322c
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-09-30 19:51:49 +0200

    Create main page and add dependencies (D3, JQuery, main script file)

---
+++ js/charting.js
@@ -0,0 +1,14 @@
+var main = function ()
+{
+    "use strict";
+    
+    var chart = document.getElementById('chart');
+    
+    /*Plotly.plot( chart, [{
+        x: [1,2,3],
+        y: [1,2,4]
+    }]);*/
+}
+
+// Define entry point for program (main function in this case)
+$(document).ready(main);
